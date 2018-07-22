a28b06456f02618b11d97fe939d6653282472a4c
Malek Itani <malek@Lenovo-ideapad-110-15ISK>
2018-07-22 11:19:36 +0300

    Added state names

--- Main.vhd
+++ Main.vhd
@@ -8,5 +8,13 @@ entity vending_machine_interface is
 		money_entered: IN std_logic_vector (4 downto 0);
 		options: IN std_logic_vector(1 downto 0);
 		--display: OUT s
-		coordinates_xy: OUT std_logic_vector(6 downto 0) ); 
+		coordinates_xy: OUT std_logic_vector(6 downto 0) );
 end vending_machine_interface;
+
+
+architecture vm_arch of vending_machine_interface is
+	type STATE is (ENTER_MONEY, SELECT_ITEM, PRE_DISPENSE, POST_DISPENSE);
+	begin
+		
+
+end vm_arch;
